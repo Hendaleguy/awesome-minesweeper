08b2268ec160e6845061ebc2a3efca59229e722d
Hank <hendaleguy@gmail.com>
2025-08-05 21:56:06 -0500

    initial project commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private bool _isMine;
+
+    private bool _isRevealed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
